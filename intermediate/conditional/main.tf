44b769cd3cf1dd23ec6363626a87d2611f179f19
mirdan.syahid@dana.id <mirdan.syahid@dana.id>
2024-04-20 23:14:43 +0700

    update

--- intermediate/conditional/main.tf
+++ intermediate/conditional/main.tf
@@ -1,12 +1,19 @@
 variable "env" {
   description = "Deployment environment (prod or dev)"
   type        = string
-  default     = "dev"
+  default     = "staging"
+}
+
+variable "instance_count" {
+  description = "Number of instances to launch"
+  type        = number
+  default     = 1
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-015f72d56355ebc27"
   instance_type = var.env == "prod" ? "t2.large" : "t2.micro"
+  count         = var.env == "prod" ? 2 : var.instance_count
 
   tags = {
     Name = "Example-${var.env}"
