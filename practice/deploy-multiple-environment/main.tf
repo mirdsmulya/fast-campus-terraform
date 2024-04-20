0512742bbf889806147355841e085d5f21d563b0
mirdan.syahid@dana.id <mirdan.syahid@dana.id>
2024-04-20 16:58:16 +0700

    Add multiple env

---
+++ practice/deploy-multiple-environment/main.tf
@@ -0,0 +1,30 @@
+# backend.tf
+terraform {
+    backend "s3" {
+        bucket         = "practice-remote-state-1"
+        key            = "testing/ec2"
+        region         = "ap-southeast-1"
+        encrypt        = true
+    }
+}
+
+provider "aws" {
+  region = "ap-southeast-1" # Anda bisa mengganti ini dengan region yang diinginkan
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-015f72d56355ebc27" # AMI untuk Amazon Linux 2 di ap-southeast-1, ganti sesuai kebutuhan
+  instance_type = "t2.micro" # Tipe instance
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+
+
+
+
+
+
+
